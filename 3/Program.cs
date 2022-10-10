98b9c068850cd32c3038da8e8fbdee6f9229db12
artempetich <artempetich@yandex.ru>
2022-10-10 21:00:57 +0500

    random

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,5 +1,7 @@
-﻿int x = 31;
-int y = 52;
+﻿int x = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(x);
+int y = new Random().Next(1,10);
+Console.WriteLine(y);
 int xy = x + y;
 Console.WriteLine(xy);
 
