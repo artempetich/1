3b49ceb27bc11e05b20ca2488e18dde6c95d43be
artempetich <artempetich@yandex.ru>
2022-10-10 21:53:59 +0500

    if else

---
+++ 5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя: ");
+string x = Console.ReadLine();
+
+if(x.ToLower() == "петя")
+{
+    Console.WriteLine("Ура, это же том самый красавчик Петя!");
+}
+else
+{
+    Console.Write("Здрасти, а где Петя? ");
+    Console.WriteLine(x);
+}
